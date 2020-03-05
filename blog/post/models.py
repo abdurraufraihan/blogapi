74228233f515092388c9bc2f13375c715bd5fc5b
Abdur Rauf Raihan <abdurraufraihan@gmail.com>
2020-03-05 21:04:36 +0600

    made relation between post with category and tag

--- blog/post/models.py
+++ blog/post/models.py
@@ -1,8 +1,13 @@
 from django.db import models
+from taxonomy.models import Category, Tag
 
 class Post(models.Model):
 	title = models.CharField(max_length=50)
 	description = models.TextField()
+	category = models.ForeignKey(
+		Category, on_delete=models.SET_NULL, null=True, blank=True
+	)
+	tag = models.ManyToManyField(Tag)
 
 	def __str__(self):
 		return self.title
