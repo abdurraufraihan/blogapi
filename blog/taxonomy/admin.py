fecda652ecc5d493873ec54af99a8f8e54fb6461
Abdur Rauf Raihan <abdurraufraihan@gmail.com>
2020-03-05 21:16:30 +0600

    added category and tag to admin

--- blog/taxonomy/admin.py
+++ blog/taxonomy/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Category, Tag
 
-# Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+	list_display = ['name']
+
+class TagAdmin(admin.ModelAdmin):
+	list_display = ['name']
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Tag, TagAdmin)
