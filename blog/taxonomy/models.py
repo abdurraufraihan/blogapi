060df51d64e44fb2bb0ee75f71feef0159c873e0
Abdur Rauf Raihan <abdurraufraihan@gmail.com>
2020-03-05 21:02:42 +0600

    Category and Tag model added

--- blog/taxonomy/models.py
+++ blog/taxonomy/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+	name = models.CharField(max_length=30)
+
+	def __str__(self):
+		return self.name
+
+class Tag(models.Model):
+	name = models.CharField(max_length=30)
+
+	def __str__(self):
+		return self.name
