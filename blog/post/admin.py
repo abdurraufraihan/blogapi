928440ebd11da8e3918c1d233172d7e7555e2e99
Abdur Rauf Raihan <abdurraufraihan@gmail.com>
2020-03-05 21:20:53 +0600

    added missing id to admin for post, caegory and tag

--- blog/post/admin.py
+++ blog/post/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import Post
 
 class PostAdmin(admin.ModelAdmin):
-	list_display = ['title', 'description', 'category']
+	list_display = ['id', 'title', 'description', 'category']
 
 admin.site.register(Post, PostAdmin)
--- blog/taxonomy/admin.py
+++ blog/taxonomy/admin.py
@@ -2,10 +2,10 @@ from django.contrib import admin
 from .models import Category, Tag
 
 class CategoryAdmin(admin.ModelAdmin):
-	list_display = ['name']
+	list_display = ['id', 'name']
 
 class TagAdmin(admin.ModelAdmin):
-	list_display = ['name']
+	list_display = ['id', 'name']
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Tag, TagAdmin)
