c348af9d7bf56b6e2d375e0789cc090c08bcf80e
Abdur Rauf Raihan <abdurraufraihan@gmail.com>
2020-03-05 22:44:12 +0600

    post serializer added and post view added for post

--- blog/api/views/post/postlistview.py
+++ blog/api/views/post/postlistview.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from lib import constants as const
 from lib import errormessages as errorMessage
 from post.serializers.postresponseserializer import PostResponseSerializer
+from post.serializers.postsaveserializer import PostSaveSerializer
 from post.models import Post
 
 class PostListView(APIView):
@@ -12,6 +13,23 @@ class PostListView(APIView):
 			queryset = Post.objects.all()
 			serializer = PostResponseSerializer(queryset, many=True)
 			return Response(serializer.data, status=status.HTTP_200_OK)
+		except:
+			return Response(
+				{const.ERROR_PROPERTY : errorMessage.INTERNAL_SERVER_ERROR},
+				status=status.HTTP_500_INTERNAL_SERVER_ERROR
+			)
+
+	def post(self, request, format=None):
+		try:
+			serializer = PostSaveSerializer(data=request.data)
+			if serializer.is_valid():
+				post = serializer.save()
+				serializer = PostResponseSerializer(post)
+				return Response(serializer.data, status=status.HTTP_201_CREATED)
+			else:
+				return Response(
+					serializer.errors, status=status.HTTP_400_BAD_REQUEST
+				)
 		except:
 			return Response(
 				{const.ERROR_PROPERTY : errorMessage.INTERNAL_SERVER_ERROR},
---
+++ blog/post/serializers/postsaveserializer.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from lib import constants as const
+from post.models import Post
+
+class PostSaveSerializer(serializers.ModelSerializer):
+	def create(self, validatedData):
+		tag = validatedData.pop(const.TAG_PROPERTY)
+		post = Post.objects.create(**validatedData)
+		post.tag.set(tag)
+		return post
+
+	class Meta:
+		model = Post
+		fields = [
+			const.TITLE_PROPERTY,
+			const.DESCRIPTION_PROPERTY,
+			const.CATEGORY_PROPERTY,
+			const.TAG_PROPERTY,
+			const.IMAGE_PROPERTY
+		]
