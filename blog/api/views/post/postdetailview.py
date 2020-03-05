89b4d68f3113b0909e8cdb95e4c188e31829a87d
Abdur Rauf Raihan <abdurraufraihan@gmail.com>
2020-03-06 02:07:59 +0600

    post update api completed with dynamically modified field

--- blog/api/views/post/postdetailview.py
+++ blog/api/views/post/postdetailview.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from lib import constants as const
 from lib import errormessages as errorMessage
 from post.serializers.postresponseserializer import PostResponseSerializer
+from post.serializers.postupdateserializer import PostUpdateSerializer
 from post.models import Post
 
 class PostDetailView(APIView):
@@ -24,6 +25,29 @@ class PostDetailView(APIView):
 				)
 			serializer = PostResponseSerializer(post)
 			return Response(serializer.data, status=status.HTTP_200_OK)
+		except:
+			return Response(
+				{const.ERROR_PROPERTY : errorMessage.INTERNAL_SERVER_ERROR},
+				status=status.HTTP_404_NOT_FOUND
+			)
+
+	def put(self, request, pk, format=None):
+		try:
+			post = self.getPost(pk)
+			if not post:
+				return Response(
+					{const.ERROR_PROPERTY : \
+						errorMessage.POST_DOES_NOT_EXIST_ERROR},
+					status=status.HTTP_404_NOT_FOUND
+				)
+			serializer = PostUpdateSerializer(post, data=request.data)
+			if serializer.is_valid():
+				serializer.save()
+				return Response(serializer.data, status=status.HTTP_200_OK)
+			else:
+				return Response(
+					serializer.errors, status=status.HTTP_400_BAD_REQUEST
+				)
 		except:
 			return Response(
 				{const.ERROR_PROPERTY : errorMessage.INTERNAL_SERVER_ERROR},
---
+++ blog/post/serializers/postupdateserializer.py
@@ -0,0 +1,27 @@
+from lib import constants as const
+from post.serializers.postsaveserializer import PostSaveSerializer
+
+class PostUpdateSerializer(PostSaveSerializer):
+	def __init__(self, *args, **kwargs):
+		fields = None
+		if const.DATA_PROPERTY in kwargs:
+			fields = kwargs[const.DATA_PROPERTY].dict().keys()
+		super().__init__(*args, **kwargs)
+		if fields:
+			allowedFields = set(fields)
+			existingFields = set(self.fields)
+			for fieldName in existingFields - allowedFields:
+				self.fields.pop(fieldName)
+
+	def update(self, post, validatedData):
+		post.title = validatedData.get(const.TITLE_PROPERTY, post.title)
+		post.description = \
+			validatedData.get(const.DESCRIPTION_PROPERTY, post.description)
+		post.category = \
+			validatedData.get(const.CATEGORY_PROPERTY, post.category)
+		tag = validatedData.get(const.TAG_PROPERTY, post.tag)
+		if isinstance(tag, list):
+			post.tag.set(tag)
+		post.image = validatedData.get(const.IMAGE_PROPERTY, post.image)
+		post.save()
+		return post
