174195dd2ead7f141376629e65b4378281fa1245
Abdur Rauf Raihan <abdurraufraihan@gmail.com>
2020-03-07 22:44:07 +0600

    added IsAuthenticatedOrGetRequest permission for Category and Post

--- blog/api/views/taxonomy/categoryview.py
+++ blog/api/views/taxonomy/categoryview.py
@@ -1,12 +1,18 @@
 from rest_framework.generics import ListCreateAPIView, \
 	RetrieveUpdateDestroyAPIView
+from api.permissions.isauthenticatedorgetrequest import \
+	IsAuthenticatedOrGetRequest
 from taxonomy.models import Category
 from taxonomy.serializer.categoryserializer import CategorySerializer
 
 class CategoryListCreateView(ListCreateAPIView):
+	permission_classes = [IsAuthenticatedOrGetRequest]
+
 	queryset = Category.objects.all()
 	serializer_class = CategorySerializer
 
 class CategoryRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
+	permission_classes = [IsAuthenticatedOrGetRequest]
+
 	queryset = Category.objects.all()
 	serializer_class = CategorySerializer
--- blog/api/views/taxonomy/tagview.py
+++ blog/api/views/taxonomy/tagview.py
@@ -1,12 +1,18 @@
 from rest_framework.generics import ListCreateAPIView, \
 	RetrieveUpdateDestroyAPIView
+from api.permissions.isauthenticatedorgetrequest import \
+	IsAuthenticatedOrGetRequest
 from taxonomy.models import Tag
 from taxonomy.serializer.tagserializer import TagSerializer
 
 class TagListCreateView(ListCreateAPIView):
+	permission_classes = [IsAuthenticatedOrGetRequest]
+
 	queryset = Tag.objects.all()
 	serializer_class = TagSerializer
 
 class TagRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
+	permission_classes = [IsAuthenticatedOrGetRequest]
+
 	queryset = Tag.objects.all()
 	serializer_class = TagSerializer
