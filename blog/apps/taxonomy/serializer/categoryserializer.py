1962daebe019076184cd51dbb65932fdc6540b09
Abdur Rauf Raihan <abdurraufraihan@gmail.com>
2020-03-10 00:10:34 +0600

    removed unnecessary apps before lib in categoryserializer.py

--- blog/apps/taxonomy/serializer/categoryserializer.py
+++ blog/apps/taxonomy/serializer/categoryserializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from apps.lib import constants as const
+from lib import constants as const
 from apps.taxonomy.models import Category
 
 class CategorySerializer(serializers.ModelSerializer):
