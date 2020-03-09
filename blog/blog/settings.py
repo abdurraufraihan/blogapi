6c00b54a408891f5f991d893e667674404a0ebcd
Abdur Rauf Raihan <abdurraufraihan@gmail.com>
2020-03-10 00:11:31 +0600

    added apps before post, taxonomy and api to INSTALLED_APPS

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -39,9 +39,9 @@ INSTALLED_APPS = [
 	'django.contrib.staticfiles',
 	'rest_framework',
 	'rest_framework.authtoken',
-	'post',
-	'taxonomy',
-	'api'
+	'apps.post',
+	'apps.taxonomy',
+	'apps.api'
 ]
 
 MIDDLEWARE = [
