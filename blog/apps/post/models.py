9d09d4bc9039010b91d853dc667c3c64c8ee3907
Abdur Rauf Raihan <abdurraufraihan@gmail.com>
2020-07-14 23:01:00 +0600

    __str__ method added to Comment model

--- blog/apps/post/models.py
+++ blog/apps/post/models.py
@@ -1,6 +1,7 @@
 import uuid
 from django.db import models
 from lib import constants as const
+from lib import commonutility as commonUtil
 from apps.taxonomy.models import Category, Tag
 
 class Post(models.Model):
@@ -24,3 +25,8 @@ class Comment(models.Model):
 	post = models.ForeignKey(
 		Post, on_delete=models.CASCADE, related_name=const.COMMENTS_PROPERTY
 	)
+
+	def __str__(self):
+		return commonUtil.getSummaryText(
+			self.description, const.COMMENT_DESCRIPTION_SUMMARY_LENGTH
+		)
