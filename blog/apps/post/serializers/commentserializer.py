a6ed3c4bea04822bf672832a65ca8ab9a2e7f888
Abdur Rauf Raihan <abdurraufraihan@gmail.com>
2020-07-16 01:03:58 +0600

    replaced field commentId with id of CommentSerializer

--- blog/apps/post/serializers/commentserializer.py
+++ blog/apps/post/serializers/commentserializer.py
@@ -3,9 +3,11 @@ from lib import constants as const
 from apps.post.models import Comment
 
 class CommentSerializer(serializers.ModelSerializer):
+	id = serializers.UUIDField(source=const.COMMENT_ID_PROPERTY, read_only=True)
+
 	class Meta:
 		model = Comment
 		fields = [
-			const.COMMENT_ID_PROPERTY,
+			const.ID_PROPERTY,
 			const.DESCRIPTION_PROPERTY
 		]
