552523cfed2e943db3f7e1889db74990fdacd0e4
Abdur Rauf Raihan <abdurraufraihan@gmail.com>
2020-07-16 23:09:32 +0600

    dispath method added to CommentListCreateView for checking postId is exist or not

--- blog/apps/api/views/comment/commentlistcreateview.py
+++ blog/apps/api/views/comment/commentlistcreateview.py
@@ -1,11 +1,23 @@
+from django.http import JsonResponse
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
 from lib import errorutility as errorUtil
-from apps.post.models import Comment
+from lib import constants as const, errormessages as errorMessage
+from apps.post.models import Post, Comment
 from apps.post.serializers.commentserializer import CommentSerializer
 
 class CommentListCreateView(APIView):
+	def dispatch(self, request, postId, *args, **kwargs):
+		isPostIdExist = Post.objects.filter(postId=postId).exists()
+		if not isPostIdExist:
+			return JsonResponse(
+				{const.ERROR_PROPERTY:
+					errorMessage.POST_ID_DOES_NOT_EXIST_ERROR},
+				status=status.HTTP_404_NOT_FOUND
+			)
+		return super().dispatch(request, postId, *args, **kwargs)
+
 	def get(self, request, postId, format=None):
 		try:
 			comments = Comment.objects.filter(post__postId=postId)
--- blog/lib/errormessages.py
+++ blog/lib/errormessages.py
@@ -1,2 +1,3 @@
 INTERNAL_SERVER_ERROR = 'Internal server error'
 POST_DOES_NOT_EXIST_ERROR = 'Post does not exist'
+POST_ID_DOES_NOT_EXIST_ERROR = 'postId does not exist'
