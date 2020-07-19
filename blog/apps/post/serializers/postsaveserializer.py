14c6dd895874db9064f2219a3d9b9488819af70c
Abdur Rauf Raihan <abdurraufraihan@gmail.com>
2020-07-20 01:27:45 +0600

    refactor post create api for category and tag uuid

--- blog/apps/post/serializers/postsaveserializer.py
+++ blog/apps/post/serializers/postsaveserializer.py
@@ -1,13 +1,24 @@
 from rest_framework import serializers
 from lib import constants as const
-from apps.post.models import Post
+from apps.post.models import Post, Category, Tag
 
 class PostSaveSerializer(serializers.ModelSerializer):
+	category = serializers.UUIDField(required=False)
+	tag = serializers.ListField(child=serializers.UUIDField(), required=False)
+
 	def create(self, validatedData):
-		tag = validatedData.pop(const.TAG_PROPERTY)
+		categoryId = None
+		if const.CATEGORY_PROPERTY in validatedData:
+			categoryId = validatedData[const.CATEGORY_PROPERTY]
+			category = Category.objects.get(categoryId=categoryId)
+			validatedData[const.CATEGORY_PROPERTY] = category
+		tags = None
+		if const.TAG_PROPERTY in validatedData:
+			tags = validatedData.pop(const.TAG_PROPERTY)
+			tags = [Tag.objects.get(tagId=tag) for tag in tags]
 		post = Post.objects.create(**validatedData)
-		if isinstance(tag, list):
-			post.tag.set(tag)
+		if tags and isinstance(tags, list):
+			post.tag.set(tags)
 		return post
 
 	class Meta:
@@ -19,3 +30,6 @@ class PostSaveSerializer(serializers.ModelSerializer):
 			const.TAG_PROPERTY,
 			const.IMAGE_PROPERTY
 		]
+		extra_kwargs = {
+			const.IMAGE_PROPERTY: {const.REQUIRED_PROPERTY: False}
+		}
