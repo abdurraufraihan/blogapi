a84be099fc8dd1ed4e6072b20db2311743e7b5f5
Abdur Rauf Raihan <abdurraufraihan@gmail.com>
2020-07-21 01:34:51 +0600

    fixed response representation of post update api

--- blog/apps/post/serializers/postupdateserializer.py
+++ blog/apps/post/serializers/postupdateserializer.py
@@ -1,11 +1,12 @@
 from lib import constants as const
 from apps.post.serializers.postsaveserializer import PostSaveSerializer
+from apps.post.models import Category, Tag
 
 class PostUpdateSerializer(PostSaveSerializer):
 	def __init__(self, *args, **kwargs):
 		fields = None
 		if const.DATA_PROPERTY in kwargs:
-			fields = kwargs[const.DATA_PROPERTY].dict().keys()
+			fields = kwargs[const.DATA_PROPERTY].keys()
 		super().__init__(*args, **kwargs)
 		if fields:
 			allowedFields = set(fields)
@@ -13,7 +14,26 @@ class PostUpdateSerializer(PostSaveSerializer):
 			for fieldName in existingFields - allowedFields:
 				self.fields.pop(fieldName)
 
+	def to_representation(self, post):
+		if const.TAG_PROPERTY in self.fields:
+			self.fields.pop(const.TAG_PROPERTY)
+			responseData = super().to_representation(post)
+			responseData[const.TAG_PROPERTY] = \
+				[tag.name for tag in post.tag.all()]
+			return responseData
+		return super().to_representation(post)
+
 	def update(self, post, validatedData):
+		categoryId = None
+		if const.CATEGORY_PROPERTY in validatedData:
+			categoryId = validatedData[const.CATEGORY_PROPERTY]
+			category = Category.objects.get(categoryId=categoryId)
+			validatedData[const.CATEGORY_PROPERTY] = category
+		tags = None
+		if const.TAG_PROPERTY in validatedData:
+			tags = validatedData.pop(const.TAG_PROPERTY)
+			tags = [Tag.objects.get(tagId=tag) for tag in tags]
+			validatedData[const.TAG_PROPERTY] = tags
 		post.title = validatedData.get(const.TITLE_PROPERTY, post.title)
 		post.description = \
 			validatedData.get(const.DESCRIPTION_PROPERTY, post.description)
